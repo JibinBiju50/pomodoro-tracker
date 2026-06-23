43c860eaa0ccfa295f4049ddd3d6942f2e3215b5
Jibin <Jibinchelakattu555@gmail.com>
2026-06-23 20:56:47 +0530

    Fix navbar layout issue on login and register.

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,10 +2,12 @@ import Navbar from "../components/Navbar";
 import Register from "../components/Register";
 
 export default function RegisterPage() {
-    return(
-        <div className="min-h-screen bg-gray-700 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 py-4 flex flex-col justify-center">
+    return (
+        <div className="min-h-screen bg-gray-700 flex flex-col">
             <Navbar />
-            <Register />
+            <div className="px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 py-4 flex flex-col justify-center flex-1">
+                <Register />
+            </div>
         </div>
     )
 }
